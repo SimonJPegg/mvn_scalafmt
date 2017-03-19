6049c9a6247cee21ce65f6c9a428d9a41714f1e8
Ciaran Kearney <C.Kearney@kainos.com>
2017-03-19 12:28:41 +0000

    develop: initial version

---
+++ src/main/scala/org/antipathy/scalafmtmvn/Formatter.scala
@@ -0,0 +1,20 @@
+package org.antipathy.scalafmtmvn
+
+import org.scalafmt.Error.UnableToParseCliOptions
+import org.scalafmt.cli.{Cli, CliOptions}
+/**
+  * Gets calls scalafmt with the config file specified at configLocation
+  */
+object Formatter {
+
+  /**
+    * Gets calls scalafmt with the config file specified at configLocation
+    * @param configLocation the location of a scalafmt.conf file
+    */
+  def format(configLocation: String): Unit = {
+    Cli.getConfig(Array[String]("--config",configLocation),CliOptions.default) match {
+      case Some(x) => Cli.run(x)
+      case None => throw UnableToParseCliOptions
+    }
+  }
+}
