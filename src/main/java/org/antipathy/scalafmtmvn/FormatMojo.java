5e2ed59d2a6f8508c45b326591a3dc793300b8d8
jbkt <jbcatte@gmail.com>
2017-07-27 22:16:15 +0200

    Fix NPE and wrong formatting options: (#12)
    * Fix NPE and wrong formatting options:
    
    - NullPointerException when no parameters in Maven configuration.
    - All files in current directory formatted instead of all files
      in current project.
    
    * Fix NPE and wrong formatting options:
    
    - Update tests and README

--- src/main/java/org/antipathy/scalafmtmvn/FormatMojo.java
+++ src/main/java/org/antipathy/scalafmtmvn/FormatMojo.java
@@ -5,6 +5,9 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.List;
 
 /**
  * Get the location of the config file and pass to Formatter
@@ -12,24 +15,33 @@ import org.apache.maven.plugins.annotations.Parameter;
 @Mojo(name = "format")
 public class FormatMojo extends AbstractMojo {
 
-    @Parameter(property="format.configLocation",defaultValue="")
+    @Parameter(property = "format.configLocation")
     private String configLocation;
-    @Parameter(property="format.parameters",defaultValue="")
+    @Parameter(property = "format.parameters")
     private String parameters;
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
 
     public void execute() throws MojoExecutionException {
-        if(StringUtils.isEmpty(configLocation)) {
+        if (StringUtils.isBlank(configLocation)) {
             throw new MojoExecutionException("No configuration file specified");
+        }
+        getLog().info("Formatting with config: " + configLocation);
+
+        if (!StringUtils.isBlank(parameters)) {
+            getLog().info(" and options: " + parameters);
         } else {
-            getLog().info("Formatting with config: " + configLocation);
-            if (parameters != null) {
-                getLog().info(" and options: " + parameters);
-            }
-            try {
-                Formatter.format(configLocation, parameters);
-            } catch (Exception e) {
-                throw new MojoExecutionException("Error formatting Scala files", e);
-            }
+            parameters = "";
+        }
+
+        List<String> sourceRoots = project.getCompileSourceRoots();
+        String[] sources = (sourceRoots == null) ? new String[0] : sourceRoots.toArray(new String[sourceRoots.size()]);
+        getLog().info(" sources: " + sources);
+
+        try {
+            Formatter.format(configLocation, parameters, sources);
+        } catch (Exception e) {
+            throw new MojoExecutionException("Error formatting Scala files", e);
         }
     }
 }
