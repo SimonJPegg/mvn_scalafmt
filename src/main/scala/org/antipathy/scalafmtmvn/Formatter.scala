f6f7273e486ee491e6c2890244a733b1d31e9bee
Jean-Baptiste Catté <jb.catte@criteo.com>
2018-02-06 15:08:20 +0000

    Make configuration file optional

--- src/main/scala/org/antipathy/scalafmtmvn/Formatter.scala
+++ src/main/scala/org/antipathy/scalafmtmvn/Formatter.scala
@@ -15,18 +15,18 @@ object Formatter {
     * @param configLocation the location of a scalafmt.conf file
     */
   def format(configLocation: String, parameters: String, sourceLocations: Array[String]): Unit = {
-    val params: Seq[String] = parameters match {
-      case "" => Seq("--config", configLocation)
-      case _: String => parameters.split(" ") ++ Seq("--config", configLocation)
-    }
+    val emptySeq = Seq[String]()
+
+    val params: Seq[String] = if (parameters == null) emptySeq else parameters.split(" ")
+    val config: Seq[String] = if (configLocation == null) emptySeq else Seq("--config", configLocation)
+
     val sourcePaths = sourceLocations.flatMap(path => AbsoluteFile.fromPath(path))
 
     val options = CliOptions(customFiles = sourcePaths)
 
-    Cli.getConfig(params.toArray, options) match {
+    Cli.getConfig((params ++ config).toArray, options) match {
       case Some(x) => Cli.run(x)
-      case None =>
-        throw new IllegalArgumentException(s"unable to parse config: $configLocation", UnableToParseCliOptions)
+      case None => Cli.run(options)
     }
   }
 }
