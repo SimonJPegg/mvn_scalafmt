069f334739cc891def1367d6a14e457381f64ad4
Eugene Platonov <jozic@live.com>
2019-10-16 16:13:58 -0400

    Fail build if Scalafmt reports errors (#43)

---
+++ src/test/scala/org/antipathy/mvn_scalafmt/logging/MavenLogReporterSpec.scala
@@ -0,0 +1,37 @@
+package org.antipathy.mvn_scalafmt.logging
+
+import java.io.File
+
+import org.apache.maven.monitor.logging.DefaultLog
+import org.codehaus.plexus.logging.console.ConsoleLogger
+import org.scalafmt.dynamic.exceptions.ScalafmtException
+import org.scalatest.{FlatSpec, GivenWhenThen, Matchers}
+
+class MavenLogReporterSpec extends FlatSpec with GivenWhenThen with Matchers {
+
+  val reporter = new MavenLogReporter(new DefaultLog(new ConsoleLogger()))
+
+  behavior of "MavenLogReporter"
+
+  it should "throw an error if error is reported by Scalafmt" in {
+
+    val ex1 = intercept[ScalafmtException] {
+      reporter.error(new File("").toPath, "Oops")
+    }
+
+    ex1.getMessage shouldEqual "Oops"
+
+    val ex2 = intercept[RuntimeException] {
+      reporter.error(new File("").toPath, "No way!", new RuntimeException("Oops"))
+    }
+
+    ex2.getMessage shouldEqual "No way!"
+
+    val ex3 = intercept[RuntimeException] {
+      reporter.error(new File("").toPath, new RuntimeException("Oops"))
+    }
+
+    ex3.getMessage shouldEqual "Oops"
+  }
+
+}
